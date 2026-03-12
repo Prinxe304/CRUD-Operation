bc7a3df13e31784e4a0dd2e81ab5ae269025043c
Prince Hirpara <124500048+Prinxe304@users.noreply.github.com>
2026-03-12 21:49:04 +0530

    Delete clss directory

--- clss/src/main/java/com/aaru/st1/clss/repo/Studentrepo.java
+++
@@ -1,7 +0,0 @@
-package com.aaru.st1.clss.repo;
-
-import com.aaru.st1.clss.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Studentrepo extends JpaRepository<Student, Long> {
-}
